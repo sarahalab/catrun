1ea5365b0558ac27553ba92a4502909908ddb202
dernah <144572300+dernah@users.noreply.github.com>
2023-10-11 07:27:50 -0400

    Spikes now respawn player

--- 487-catrun/Assets/scripts/SpikeDamage.cs
+++ 487-catrun/Assets/scripts/SpikeDamage.cs
@@ -1,13 +1,24 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpikeDamage : MonoBehaviour
 {
     [SerializeField] private float damage;
+    public int Respawn;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             collision.GetComponent<Health>().TakeDamage(damage);
+            StartCoroutine(DelayedRespawn(Respawn));
+        }
+    }
+    private IEnumerator DelayedRespawn(int sceneIndex)
+    {
+        {
+            yield return new WaitForSeconds(2.0f); // Waits 2 seconds to respawn the player
+            SceneManager.LoadScene(sceneIndex);
         }
     }
 }
